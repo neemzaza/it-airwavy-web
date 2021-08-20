e17837dfae4cf05caca5eaa63ea0e4e3d16f35eb
Sarut <airwavy.442511@gmail.com>
2021-08-20 13:03:06 +0700

    reactbeta 6 pop

--- src/Pop.jsx
+++ src/Pop.jsx
@@ -5,6 +5,7 @@ import defaultEmpop from './img/3emdis.png'
 import Empop from './img/3empop.png'
 
 export default function Pop() {
+    $("body").css("overflow", "hidden")
     const getCookie = () => {
         let HelloWorld = document.cookie.split('clickTime=');
         let getNum = HelloWorld[1]
@@ -13,23 +14,26 @@ export default function Pop() {
     }
     
     const [clickTime, addClickTime] = useState(!document.cookie ? 0 : parseInt(getCookie()));
+    const [totalClick, setTotalClick] = useState(0)
 
     // console.log(clickTime)
 
     const click = () => {
         addClickTime(parseInt(clickTime + 1));
-        document.cookie = "clickTime=" + clickTime;
+        document.cookie = "clickTime=" + (clickTime + 1);
         $("#pluem").css({
             "background": `url('${Empop}')`,
-            "background-size": "45rem",
+            "background-size": "30rem",
             "background-position": "center center",
             "background-repeat": "no-repeat",
             "width": "100vw",
             "height": "100vh",
         })
 
-        axios.post('http://localhost:5000/pop', { clickTime: clickTime, team: "Airwavy" }, (res) => {
-
+        axios.post('http://localhost:5000/pop', { clickTime: 1, team: "Airwavy" }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
         })
 
         // getCookie()
@@ -39,19 +43,31 @@ export default function Pop() {
     const notClick = () => {
         $("#pluem").css({
             "background": `url('${defaultEmpop}')`,
-            "background-size": "45rem",
+            "background-size": "30rem",
             "background-position": "center center",
             "background-repeat": "no-repeat",
             "width": "100vw",
             "height": "100vh",
         })
     }
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/').then(res => {
+            setTotalClick(res.data)
+        })
+    }, [])
+
+    setTimeout(() => {
+        axios.get('http://localhost:5000/').then(res => {
+            setTotalClick(res.data)
+        })
+    }, 15000);  
     return (
-        <div className="">
-            <div className="pluem" id="pluem" tabIndex="0" onKeyDown={() => click()} onKeyUp={() => notClick()} onMouseDown={() => click()} onMouseUp={() => notClick()}>
+        <div className="mainPage popPluem" tabIndex="0" onKeyDown={() => click()} onKeyUp={() => notClick()}>
+            <div className="pluem" id="pluem" tabIndex="0" onTouchStart={() => click()} onMouseDown={() => click()} onMouseUp={() => notClick()}>
                 <center>
-                    <br/><br/><br/><br/>
-                    <h1 className="text-white bigprime">PopPluem</h1>
+                    <br/><br/><center><p className="text-white">การคลิกของคนทั้งหมด : {totalClick} (ค่าจะอัปเดตทุกๆ 15 วินาที)</p></center><br/>
+                    <h1 className="text-white bigprime stroketext">PopPluem</h1>
                     <h1 className="text-white">{clickTime}</h1>
                 </center>
             </div>
